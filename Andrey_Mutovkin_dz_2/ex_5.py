72ffa4445af7e4d0260fa7a4d753f413232ec039
Eulampios <88624719+Eulampios@users.noreply.github.com>
2021-10-06 22:18:43 +0300

    write code (edit)

--- Andrey_Mutovkin_dz_2/ex_5.py
+++ Andrey_Mutovkin_dz_2/ex_5.py
@@ -4,17 +4,17 @@ for i in list:
     kop = (i - int(i)) * 100
     print(f'{rub} руб {kop:02.0f} коп')
 
-list = [23.34, 4.51, 87, 23, 55.6, 45, 91.35, 54, 741, 251, 342, 345, 65, 44, 5, 55, 9]
-print(id(list))
+s_list = [23.34, 4.51, 87, 23, 55.6, 45, 91.35, 54, 741, 251, 342, 345, 65, 44, 5, 55, 9]
+print(id(s_list))
 list.sort()
-print(id(list))
-for i in list:
+print(id(s_list))
+for i in s_list:
     rub = int(i)
     kop = (i - int(i)) * 100
     print(f'{rub} руб {kop:02.0f} коп')
 
-list = [23.34, 4.51, 87, 23, 55.6, 45, 91.35, 54, 741, 251, 342, 345, 65, 44, 5, 55, 9]
-for k in sorted(list)[::-1][:5]:
+s_list = [23.34, 4.51, 87, 23, 55.6, 45, 91.35, 54, 741, 251, 342, 345, 65, 44, 5, 55, 9]
+for k in sorted(s_list)[::-1][:5]:
     rub = int(k)
     kop = (k - int(k)) * 100
     print(f'{rub} руб {kop:02.0f} коп')
