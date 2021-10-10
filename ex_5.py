aaf24f5cceab0397e72f8ef4dfdb70722c4bbb29
Eulampios <88624719+Eulampios@users.noreply.github.com>
2021-10-10 20:40:59 +0300

    write code

--- ex_5.py
+++ ex_5.py
@@ -4,7 +4,11 @@ nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
 adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
+
 def jokes(num):
+    '''функция будет генерировать шуктку из 3 слов, используя 3 словаря
+    :param num: введите количество слов
+    :return: Вы, все-таки, увидите шутку ☻'''
     joke_list = []
     for i in range(num):
         cur_nouns = random.choice(nouns)
@@ -12,6 +16,7 @@ def jokes(num):
         cur_adjectives = random.choice(adjectives)
         joke_list.append(f'{cur_nouns} {cur_adverbs} {cur_adjectives}')
     return joke_list
+
 
 print(jokes(4))
 print(jokes(3))
