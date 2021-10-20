38b35411aa9a4c61a3159cdd2920fd231d601ed6
Eulampios <88624719+Eulampios@users.noreply.github.com>
2021-10-20 22:15:49 +0300

    edit code

--- Andrey_Mutovkin_dz_6/ex_3.py
+++ Andrey_Mutovkin_dz_6/ex_3.py
@@ -5,9 +5,11 @@ with open('total.txt', 'w', encoding='utf-8') as tot_file:
         line_users = [i.strip() for i in file3]
         with open('hobby.csv', encoding='utf-8') as file5:
             line_hobby = [i.strip() for i in file5]
+            my_dict = dict(zip(line_users, line_hobby))
             if len(line_users) > len(line_hobby):
-                my_dict = dict(zip(line_users, line_hobby))
-                tot_file.write(str(my_dict.setdefault(line_users[-1])))
+                new_dict = dict.fromkeys(line_users[len(line_hobby):], None)
+                my_dict.update(new_dict)
+                tot_file.write(str(my_dict))
             elif len(line_users) < len(line_hobby):
                 tot_file.write(str(sys.exit(1)))
             else:
